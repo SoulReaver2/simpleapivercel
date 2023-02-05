24abce350158cb6541d0e1554490c859cdb3e7ee
Your Name <you@example.com>
2023-02-05 22:25:05 +0100

    part2 tune data query

--- api/mails.js
+++ api/mails.js
@@ -4,11 +4,9 @@ export default async function handler(request, response) {
   const { database } = await connectToDatabase();
   const collection = database.collection(process.env.NEXT_ATLAS_COLLECTION);
 
-  const options = {
-    sort: { timestamp: -1 }
-  };
+  const sort = { timestamp: -1 };
 
-  const results = await collection.find().sort(options).toArray();
+  const results = await collection.find({}).sort(sort).toArray();
 
   response.status(200).json(results);
 }
